9650dc783e28fa84eec5145769dcc2b293660ea7
vatsal-panchal <panchalvatsal2025@gmail.com>
2026-09-18 14:11:19 +0530

    code

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,6 @@
+import userModel from "../models/user.model.js"
+
+
+export const registerController = async () => {
+    
+}
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { registerController } from "../controllers/auth.controller.js";
+
+
+const router = Router()
+
+router.post("/register",registerController)
+
+
+export default router

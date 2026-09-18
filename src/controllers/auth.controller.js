902d7e60d2166feafc524f5c44a65ffbf3d11cad
vatsal-panchal <panchalvatsal2025@gmail.com>
2026-09-18 15:08:11 +0530

    code

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,28 @@
-import userModel from "../models/user.model.js"
+import userModel from "../models/user.model.js";
 
+export const registerController = async (req, res) => {
+  const { email, phone, password } = req.body;
 
-export const registerController = async (req,res) => {
- 
-    const {email,phone,password} = req.body
+  const errors = [];
 
-    
-    
-}
+  if (!email) {
+    errors.push({
+      field: "email",
+      message: "email is required",
+    });
+  }
+
+  if (!phone) {
+    errors.push({
+      field: "phone",
+      message: "phone is required",
+    });
+  }
+
+  if (!password) {
+    errors.push({
+      field: "password",
+      message: "password is required",
+    });
+  }
+};
